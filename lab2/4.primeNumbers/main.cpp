0c2241705b852087d74bf73d3cf581d3ce065d35
kelvionald <31734063+kelvionald@users.noreply.github.com>
2020-03-21 00:02:33 +0300

    2.4 программа для вывода простых чисел

--- lab2/4.primeNumbers/main.cpp
+++ lab2/4.primeNumbers/main.cpp
@@ -1,9 +1,35 @@
 ﻿#include "pch.h"
 #include <iostream>
+#include <sstream>
 
 using namespace std;
 
-int main()
-{
+void DisplayError(string message) {
+	cout << message << endl << "Usage: primeNumbers.exe <max number>\n";
+}
 
+int main(int argc, char* argv[])
+{
+	if (argc != 2)
+	{
+		DisplayError("Invalid arguments count");
+		return 1;
+	}
+	int upperBound;
+	stringstream ss;
+	ss << argv[1];
+	if (ss >> upperBound)
+	{
+		set<int> primes = GeneratePrimeNumbersSet(upperBound);
+		for (int prime : primes)
+		{
+			cout << prime << ' ';
+		}
+		return 0;
+	}
+	else
+	{
+		DisplayError("Argument <max number> is not numeric");
+		return 1;
+	}
 }
