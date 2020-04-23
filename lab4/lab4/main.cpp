c3524565c24b534a888b0c5539f62c9cd89a7397
kelvionald <31734063+kelvionald@users.noreply.github.com>
2020-04-23 13:53:59 +0300

    4.1 программа

--- lab4/lab4/main.cpp
+++ lab4/lab4/main.cpp
@@ -1,6 +1,35 @@
 ﻿#include <iostream>
+#include "Program.h"
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	Program program;
+	try
+	{
+		program.ReadShapes(cin);
+		IShape* bigAreaShape = program.FindWithMaxArea();
+		IShape* smallPerimeterShape = program.FindWithMinPerimeter();
+		if (bigAreaShape != nullptr)
+		{
+			cout << "Shape with max area: " << bigAreaShape->ToString() << endl;
+		}
+		else
+		{
+			cout << "Shape with max area not found" << endl;
+		}
+		if (smallPerimeterShape != nullptr)
+		{
+			cout << "Shape with min perimeter: " << smallPerimeterShape->ToString() << endl;
+		}
+		else
+		{
+			cout << "Shape with min perimeter not found" << endl;
+		}
+	} 
+	catch (exception& ex)
+	{
+		cout << "Error: " << ex.what() << endl;
+	}
 }
